6831794f4211ef84cb7cd951cb9c652a07536a53
ShadesAndGrays <totoobieke@gmail.com>
2024-03-14 23:02:13 +0100

    Updated rcat to take multiple files

--- src/bin/rcat.rs
+++ src/bin/rcat.rs
@@ -1,20 +1,26 @@
 use std::{fs::File, io::Read};
-
 use clap::Parser;
 
 #[derive(Parser)]
 struct Args{
-    #[arg(short,long)]
-    file : String
+    #[arg(short,long,num_args=0..)]
+    file: Vec<String>
 }
 
 fn main() {
     let args = Args::parse();
-
-    let mut file = File::open(args.file).unwrap();
     let mut content = String::new();
-    
-    file.read_to_string(&mut content).unwrap();
+    for i in args.file{
+        match File::open(&i){
+            Ok(mut value) => {
+                value.read_to_string(&mut content).unwrap();
+                println!("{content}");},
+            Err(_) => {
+                println!("rcat: {} :No such file or directory",&i);
+                continue;
+            }
 
-    println!("{content}");
+        };
+
+    }
 }
